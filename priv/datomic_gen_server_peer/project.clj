28af7cb2aff97b7765b0bccb70ecc9fff01f4636
Paul Blair <psfblair@gmail.com>
2016-03-09 15:52:33 -0500

    Update Datomic jar.

--- priv/datomic_gen_server_peer/project.clj
+++ priv/datomic_gen_server_peer/project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [clojure-erlastic "0.3.0"]
-                 [com.datomic/datomic-pro "0.9.5344"]
+                 [com.datomic/datomic-pro "0.9.5350"]
                  [vvvvalvalval/datomock "0.1.0"]
                  [net.phobot.datomic/seed "3.0.0"]
                 ]
